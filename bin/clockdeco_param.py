d77a68b17a5d1f1926a71be964184a4c08940f20
Peu1980 <120436163+Peu1980@users.noreply.github.com>
2023-11-18 02:16:53 -0500

    Update clockdeco_param.py

--- bin/clockdeco_param.py
+++ bin/clockdeco_param.py
@@ -14,10 +14,10 @@ def clock(fmt=DEFAULT_FMT):
         return clocked
     return decorate
 
-#@clock()
-#def snooze5111(seconds):
-#    time.sleep(seconds)
-#    return(str(seconds))
+@clock()
+def snooze5111(seconds):
+    time.sleep(seconds)
+    return(str(seconds))
 
 if __name__ == '__main__':
 
