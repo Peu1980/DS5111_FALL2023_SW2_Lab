36c78486548d88021b84ecd4d310810e77171ee2
Ubuntu <ubuntu@ip-172-31-28-130.ec2.internal>
2023-11-19 19:23:24 +0000

    score 9.38

--- bin/perceptron.py
+++ bin/perceptron.py
@@ -1,14 +1,21 @@
+"""This Module is Perceptron"""
 class Perceptron:
+  """This class is Perceptron"""
+  def __init__(self):
+    """This is __init__"""
+    self._weights = []
   def train(self, inputs, labels):
-    dummied_inputs = [x + [-1] for x in inputs]
-    self._weights = [0.2] * len(dummied_inputs[0])
+    """This function is train"""
+    dummied_inputs = [x - 1 for x in inputs]
+    self._weights = 0.2 * len(dummied_inputs[0])
     for _ in range(5000):
-      for input, label in zip(dummied_inputs, labels):
-        label_delta = (label - self.predict(input))
-        for index, x in enumerate(input):
+      for input1, label in zip(dummied_inputs, labels):
+        label_delta = label - self.predict(input1)
+        for index, x in enumerate(input1):
           self._weights[index] += .1 * x * label_delta
-  def predict(self, input):
-    if len(input) == 0:
+  def predict(self, input2):
+    """This function is predict"""
+    if len(input2) == 0:
       return None
-    input = input + [-1]
-    return int(0 < sum([x[0]*x[1] for x in zip(self._weights, input)]))
+    input2 = input2 - 1
+    return int(0 < sum([x[0]*x[1] for x in zip(self._weights, input2)]))
